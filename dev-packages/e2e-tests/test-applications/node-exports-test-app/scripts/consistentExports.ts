5a5d7cff51e2a1af48bd6ca67bd0c2d143434cc3
Francesco Novy <francesco.novy@sentry.io>
2024-03-12 08:18:19 +0000

    fix e2e test

--- dev-packages/e2e-tests/test-applications/node-exports-test-app/scripts/consistentExports.ts
+++ dev-packages/e2e-tests/test-applications/node-exports-test-app/scripts/consistentExports.ts
@@ -79,7 +79,7 @@ const DEPENDENTS: Dependent[] = [
   },
   {
     package: '@sentry/remix',
-    compareWith: nodeExperimentalExports,
+    compareWith: nodeExports,
     exports: Object.keys(SentryRemix),
   },
   {
